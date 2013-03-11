aaee2ebf82e31cb821590ad2cf8dc9c9f2864392
Christopher Poole <mail@christopherpoole.net>
2013-03-12 08:25:33 +1000

    added skeleton for cropping method

--- include/G4VoxelArray.hh
+++ include/G4VoxelArray.hh
@@ -61,6 +61,11 @@ class G4VoxelArrayBase {
         return index;
     };
 
+    void Crop(unsigned int x1, unsigned int x2,
+              unsigned int y1, unsigned int y2,
+              unsigned int z1, unsigned int z2) {
+
+    }
 
   public:
     unsigned int length;
