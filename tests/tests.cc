afb0b8991cc783c7ff027bcf8042795fb97a39f8
Christopher Poole <mail@christopherpoole.net>
2013-08-08 15:13:32 +1000

    added stubs for unit tests using GoogleTest

---
+++ tests/tests.cc
@@ -0,0 +1,37 @@
+//////////////////////////////////////////////////////////////////////////
+// G4VoxelData
+// ===========
+// A general interface for loading voxelised data as geometry in GEANT4.
+//
+// Author:  Christopher M Poole <mail@christopherpoole.net>
+// Source:  http://github.com/christopherpoole/G4VoxelData
+//
+// License & Copyright
+// ===================
+// 
+// Copyright 2013 Christopher M Poole <mail@christopherpoole.net>
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//////////////////////////////////////////////////////////////////////////
+
+
+#include "gtest/gtest.h"
+
+
+int _tmain(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
